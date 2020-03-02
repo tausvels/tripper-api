3dd80c44f3415d68a7d1fc5b415934e9312df63e
tausvels <engr.tausifkhan@gmail.com>
2020-03-01 20:53:37 -0800

    removed the / loading of the index

--- server.js
+++ server.js
@@ -91,9 +91,9 @@ app.use("/itineraries", itineraryRoutes(itineraryService));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 app.get("/logout", (req, res) => {
   res.clearCookie('user')
   res.end()
