caf57563ab3ed6808c8bf207bd6b53012202e6a8
Jay Seo <sjs5953@hotmail.com>
2020-03-03 16:32:48 -0800

    test

--- server.js
+++ server.js
@@ -98,8 +98,11 @@ app.get("/", (req, res) => {
   res.send('Am I there?')
 });
 app.get("/logout", (req, res) => {
+
   res.clearCookie('user')
+  res.send('Am I there?asdasdasdasdasdasdasdasdasda')
   res.end()
+
 })
 
 app.listen(PORT, () => {
